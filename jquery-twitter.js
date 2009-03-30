d8d8aa9da87fa30d082e582b22c767043fa9ff3c
James Carr <james.r.carr@gmail.com>
2009-03-30 18:56:53 -0400

    skeleton

--- jquery-twitter.js
+++ jquery-twitter.js
@@ -1,4 +1,20 @@
+function __GLOBAL_TWITTER_RESULT_CALLBACK (resp){
+        jQuery.events.trigger('stwitter_resultsRecvd', {results: resp});        
+}
 function(){
-        jQuery.searchTwitter = function(term, data, callback){
-        };
+        var searchUrl = "http://search.twitter.com/search.json";
+        
+        try{
+                jQuery.searchTwitter = function(term, data, callback){
+                        var reqUrl = searchUrl + "?q="+term;
+                        if(jQuery.isFunction(data){
+                                callback = data;
+                        };
+                        jQuery().bind('stwitter_resultsRecvd', callback);        
+                        jQuery.events.trigger('stwitter_searchSubmit', {url: reqUrl});
+
+                };
+        }catch(e){
+                throw new Exception('jQuery is not defined!');
+        }
 });
