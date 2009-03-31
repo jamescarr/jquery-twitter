33d975d2f55cffd6e7ce9dd9f139b25f955eba29
James Carr <james.r.carr@gmail.com>
2009-03-31 01:07:34 -0400

    slimmed down code, removed events

--- src/jquery-twitter.js
+++ src/jquery-twitter.js
@@ -1,30 +1,25 @@
-
-(function(){
-    var searchUrl = "http://search.twitter.com/search.json";
-    var specialKeys = ['live'];
-    try {
-        jQuery.searchTwitter = function(term, data, callback){
-            var reqUrl = searchUrl + "?q=" + term + "&callback=?";
-            if (jQuery.isFunction(data)) {
-                callback = data;
-                data = {};
-            }
-            for(var key in data){
-            	if(key != 'live')
-            		reqUrl += "&"+key + "=" + data[key];
-            }
-        
-
-            jQuery().one('stwitter_resultsRecvd', callback);
-            jQuery.event.trigger('stwitter_searchSubmit', {'url': reqUrl});
-            jQuery.getJSON(reqUrl, function(result){
-            	var event = jQuery.Event('stwitter_resultsRecvd');
-                event.results = result;
-                jQuery.event.trigger(event);
-            });
-        };
-    } 
-    catch (e) {
-        throw new Exception('jQuery is not defined!');
-    }
+( function() {
+	window.jqTwitter = {};
+	var searchUrl = "http://search.twitter.com/search.json";
+	
+	function buildUrl(term, data){
+		var reqUrl = searchUrl + "?q=" + term + "&callback=?";
+		for ( var key in data) {
+			if (key != 'live')
+				reqUrl += "&" + key + "=" + data[key];
+		}
+		return reqUrl;
+	}
+	try {
+		jQuery.searchTwitter = function(term, data, callback) {
+			var reqUrl = searchUrl + "?q=" + term + "&callback=?";
+			if (jQuery.isFunction(data)) {
+				callback = data;
+				data = {};
+			}
+			jQuery.getJSON(buildUrl(term, data), callback);
+		};
+	} catch (e) {
+		throw new Exception('jQuery is not defined!');
+	}
 })();
