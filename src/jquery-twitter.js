26fb00caf6a3e0d4c9a3ecbd66b88a15f72df9ef
James Carr <james.r.carr@gmail.com>
2009-03-31 03:04:20 -0400

    added controller object to stop the feed if desired

--- src/jquery-twitter.js
+++ src/jquery-twitter.js
@@ -4,28 +4,44 @@
 	var cache = {};
 	var searchUrl = "http://search.twitter.com/search.json";
 	
+	function Controller(){
+		this.handler = null;
+		this.paused = false;
+	}
+	Controller.prototype.stop = function(){
+		clearInterval(this.handler);
+	};
+	Controller.prototype.pause = function(){
+		this.paused = true;
+	}
+	Controller.prototype.continueFeed = function(){
+		this.pause = false
+	}
 	function buildUrl(term, data){
 		var reqUrl = searchUrl + "?q=" + term + "&callback=?";
 		for ( var key in data) {
 			if (key != 'live')
 				reqUrl += "&" + key + "=" + data[key];
-		}
+		} 
 		return reqUrl;
 	}
+	
 	try {
 		jQuery.twitter = {};
 		jQuery.twitter.liveSearch = function(term, data, callback){
+			var control = new Controller();
 			searches['last_id'+term] = 0;
-			setInterval(function(){
+			control.handler = setInterval(function(){
 				data.since_id = searches['last_id'+term];
 				jQuery.twitter.search(term, data, function(resp){
 					var results = resp.results;
 					if(results.length > 0){
 						searches['last_id'+term] = results[0].id+1;
-						callback(resp);
+						callback.call(control, resp);
 					}
 				});
 			}, 5000);
+			
 		};
 		jQuery.twitter.search = function(term, data, callback) {
 			if (jQuery.isFunction(data)) {
